46688e2f1c25c3c22329f00f2d5dcc229c535a87
jira <123644847+jira319@users.noreply.github.com>
2025-01-21 11:04:36 +0700

    test

---
+++ src/app/home/pages.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { Card, CardBody, CardHeader, Heading } from "@chakra-ui/react";
+
+
+export default function Home() {
+  return (
+    <Card>
+      <CardHeader>
+        <Heading>Home</Heading>
+      </CardHeader>
+      <CardBody>
+        
+      </CardBody>
+    </Card>
+  );
+}
